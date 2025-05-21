cfa81660c2e9d108e7cbf1b53a62fe69e6c56250
MariuszPieniakKID <pieniak@gmail.com>
2025-05-21 12:11:52 +0200

    Update server.js
    Co-Authored-By: James Andrews <2913+ja@users.noreply.github.com>

--- backend/server.js
+++ backend/server.js
@@ -30,15 +30,26 @@ app.use(cors({
 
 app.use(express.json());
 
+//const dbConfig = {
+  //host: '127.0.0.1',
+  //user: 'root',
+  //password: 'Mariusz123',
+  //database: 'wyniki_db',
+  //waitForConnections: true,
+  //connectionLimit: 10,
+  //queueLimit: 0
+//};
 const dbConfig = {
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'Mariusz123',
-  database: 'wyniki_db',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 };
+
 
 
 const pool = mysql.createPool(dbConfig);
